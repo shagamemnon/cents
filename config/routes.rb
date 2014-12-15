6759418ffa658e597b11da5a21cebb231fcead91
ByteDown <igor.yuzovitskiy@gmail.com>
2014-12-15 14:59:18 -0500

    completed devise integration including action_mailer for pw recovery

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   root 'welcome#index'
   get '/howitworks', to: 'welcome#how_it_works'
+
+  devise_for :users
+
+  resources :users, only: :show
 end
