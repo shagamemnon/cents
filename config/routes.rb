9a312cb126e7835aaa23a2557fb936cbe998ff35
ByteDown <igor.yuzovitskiy@gmail.com>
2014-12-18 17:33:33 -0500

    added routes for users, projects, and investments and also charities and projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  get 'users/show'
-
   root 'welcome#index'
-  get '/howitworks', to: 'welcome#how_it_works'
-
   devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
+  resources :users, shallow: true do
+    resources :projects do
+      resources :investments
+    end
+  end
 
-  resources :users, only: :show
+  resources :charities do
+    resources :projects, shallow: true
+  end
+
+  get '/howitworks', to: 'welcome#how_it_works'
+  get '/explore', to: 'projects#index'
 end
