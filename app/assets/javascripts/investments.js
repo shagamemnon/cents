37dc689a2b34983e4a47d2415bbbb7555a3d542e
ByteDown <igor.yuzovitskiy@gmail.com>
2015-01-16 12:16:10 -0500

    style user projects page

--- app/assets/javascripts/investments.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  var input = $(".amount-input");
-  var amountImpact = $(".amount-impact");
-  input.on('keypress', function(event) {
-      var amount = "<h1>" + this.val() + "</h1>";
-      amountImpact.append(amount);
-  });
-});
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,6 +4,7 @@ class ProjectsController < ApplicationController
   end
 
   def show
+    @user_project = Project.find(params[:id])
   end
 
   def new
